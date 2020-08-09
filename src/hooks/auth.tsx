19dae329f2f309933d818a13e340350e60b3095f
Matheus Neves <matheusneves1989@gmail.com>
2020-08-09 19:18:37 -0300

    feat(hooks): creating auth hook

---
+++ src/hooks/auth.tsx
@@ -0,0 +1,79 @@
+import React, { createContext, useCallback, useState, useContext } from 'react';
+import api from '../services/api';
+
+interface User {
+  id: string;
+  email: string;
+}
+
+interface AuthState {
+  token: string;
+  user: User;
+}
+
+interface SignInCredentials {
+  email: string;
+  password: string;
+}
+
+interface ResponseSignInData {
+  email: string;
+  id: string;
+  token: string;
+}
+
+interface AuthContextData {
+  user: User;
+  signIn(credentials: SignInCredentials): Promise<void>;
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+export const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@Navedex:token');
+    const user = localStorage.getItem('@Navedex:user');
+
+    if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
+      return {
+        token,
+        user: JSON.parse(user),
+      };
+    }
+
+    return {} as AuthState;
+  });
+
+  const signIn = useCallback(async ({ email, password }) => {
+    const response = await api.post<ResponseSignInData>('users/login', {
+      email,
+      password,
+    });
+
+    const { token, email: mail, id } = response.data;
+
+    const user = {
+      email: mail,
+      id,
+    };
+
+    localStorage.setItem('@Navedex:token', token);
+    localStorage.setItem('@Navedex:user', JSON.stringify(user));
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
+
+    setData({ token, user });
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user: data.user, signIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = (): AuthContextData => {
+  const context = useContext(AuthContext);
+  return context;
+};
