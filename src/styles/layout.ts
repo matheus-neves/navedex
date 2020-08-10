6fa28ad7a7c3123d86cd0ddd6613eb356f86bb3c
Matheus Neves <matheusneves1989@gmail.com>
2020-08-10 17:38:51 -0300

    chore: adding layout styles

---
+++ src/styles/layout.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  max-width: 1280px;
+  padding-left: 16px;
+  padding-right: 16px;
+  width: 100%;
+  margin: 0 auto;
+
+  @media (min-width: 768px) {
+    padding-left: 32px;
+    padding-right: 32px;
+  }
+`;
