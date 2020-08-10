22b33c0d1c12dc27742127246fee624c077bf649
Matheus Neves <matheusneves1989@gmail.com>
2020-08-10 14:44:11 -0300

    feat(signin): added yup validation

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,15 +1,17 @@
-import React, { useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useState } from 'react';
 import { useHistory, Link } from 'react-router-dom';
 
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErrors';
 import { useAuth } from '../../hooks/auth';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logo from '../../assets/logo.svg';
-import { Container } from './styles';
+import { Container, AnimationContainer, ErrorMessage } from './styles';
 
 interface SignInFormData {
   email: string;
@@ -17,6 +19,8 @@ interface SignInFormData {
 }
 
 const SignIn: React.FC = () => {
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
   const formRef = useRef<FormHandles>(null);
 
   const { signIn } = useAuth();
@@ -24,34 +28,74 @@ const SignIn: React.FC = () => {
 
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
-      await signIn(data);
+      try {
+        setLoading(true);
+        setErrorMessage('');
+        formRef.current?.setErrors({});
 
-      history.push('/home');
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('Digite um e-mail válido'),
+          password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        await signIn(data);
+
+        history.push('/home');
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          formRef.current?.setErrors(errors);
+          return;
+        }
+
+        setErrorMessage(
+          'Erro ao efetuar o login, verifique as credenciais digitadas e tente novamente.',
+        );
+      } finally {
+        setLoading(false);
+      }
     },
     [signIn, history],
   );
 
   return (
     <Container>
-      <img src={logo} alt="nave.rs" />
+      <AnimationContainer>
+        <img src={logo} alt="nave.rs" />
 
-      <h1>Faça seu logon</h1>
+        <h1>Faça seu logon</h1>
 
-      <Form ref={formRef} onSubmit={handleSubmit}>
-        <Input name="email" type="email" label="E-mail" placeholder="E-mail" />
-        <Input
-          name="password"
-          type="password"
-          label="Senha"
-          placeholder="Senha"
-        />
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <Input
+            name="email"
+            type="email"
+            label="E-mail"
+            placeholder="E-mail"
+          />
+          <Input
+            name="password"
+            type="password"
+            label="Senha"
+            placeholder="Senha"
+          />
 
-        <Button type="submit">Entrar</Button>
-      </Form>
+          <Button loading={loading} type="submit">
+            Entrar
+          </Button>
 
-      <p>
-        Não possui uma conta? <Link to="/cadastrar">Cadastrar</Link>
-      </p>
+          {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
+        </Form>
+
+        <p>
+          Não possui uma conta? <Link to="/cadastrar">Cadastrar</Link>
+        </p>
+      </AnimationContainer>
     </Container>
   );
 };
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,8 +1,7 @@
 import styled from 'styled-components';
+import { animateFromBottom } from '../../styles/animations';
 
 export const Container = styled.div`
-  border: 1px solid #212121;
-
   display: flex;
   flex-direction: column;
 
@@ -11,21 +10,30 @@ export const Container = styled.div`
 
   width: 100%;
   max-width: 448px;
+`;
+
+export const AnimationContainer = styled.div`
+  border: 1px solid #212121;
+
+  display: flex;
+  flex-direction: column;
   padding: 40px 32px;
+
+  animation: ${animateFromBottom} 1s;
 
   h1 {
     font-size: 18px;
-    margin: 24px 0 12px;
+    margin: 24px 0 16px;
   }
 
   img {
     height: 60px;
   }
 
-  p {
+  > p {
     font-weight: normal;
     font-size: 12px;
-    margin-top: 16px;
+    margin-top: 24px;
     text-align: center;
     color: #212121;
 
@@ -33,4 +41,11 @@ export const Container = styled.div`
       color: #424242;
     }
   }
+`;
+
+export const ErrorMessage = styled.p`
+  font-weight: 600;
+  font-size: 12px;
+  color: #c53030;
+  margin-top: 8px;
 `;
