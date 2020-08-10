ae81f72fe2a7272426da44b51270106b7937eb87
Matheus Neves <matheusneves1989@gmail.com>
2020-08-10 14:50:33 -0300

    feat(routes): added signup route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,12 +3,14 @@ import { Switch } from 'react-router-dom';
 
 import Route from './Route';
 import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
 
 import Home from '../pages/Home';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
+    <Route path="/cadastrar" component={SignUp} />
     <Route path="/home" component={Home} isPrivate />
   </Switch>
 );
