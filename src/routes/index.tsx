48c5244c69adcd2fc33c428007eff46dce7c720f
Matheus Neves <matheusneves1989@gmail.com>
2020-08-11 16:25:36 -0300

    feat(routes): added createNaver route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,7 @@ import { Switch } from 'react-router-dom';
 import Route from './Route';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import CreateNaver from '../pages/CreateNaver';
 
 import Home from '../pages/Home';
 
@@ -12,6 +13,7 @@ const Routes: React.FC = () => (
     <Route path="/" exact component={SignIn} />
     <Route path="/cadastrar" component={SignUp} />
     <Route path="/home" component={Home} isPrivate />
+    <Route path="/create-naver" component={CreateNaver} isPrivate />
   </Switch>
 );
 
